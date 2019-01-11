f9bfd308f58cdbea4f43da24cca8b7eec1a44df1
Tim Kilian <mail@timkilian.pl>
2019-01-11 18:09:27 +0100

    Add simple Plugin abstraction

---
+++ src/Plugin.js
@@ -0,0 +1,30 @@
+/**
+ * @abstract
+ */
+class Plugin {
+
+    /**
+     * @abstract
+     * @returns object
+     */
+    getMetaModel () {
+        return {};
+    }
+
+    /**
+     * @abstract
+     * @returns object
+     */
+    getStylesheet () {
+        return {};
+    }
+
+    /**
+     * @abstract
+     * @returns object
+     */
+    getToolConfiguration () {
+        return {};
+    }
+
+}
