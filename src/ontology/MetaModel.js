f6d24ba27f518f570da6cedc17d4110b0fc0914c
Tim Kilian <mail@timkilian.pl>
2019-01-15 16:23:48 +0100

    Simple import of the json object

--- src/ontology/MetaModel.js
+++ src/ontology/MetaModel.js
@@ -6,12 +6,15 @@ export class MetaModel {
     }
 
     /**
-     *
      * @param {Object} metaModel
      * @return {MetaModel}
      */
     static fromJson (metaModel) {
         const model = new MetaModel();
+        model.type = metaModel.type;
+        model.classifiers = metaModel.classifiers;
+        model.relations = metaModel.relations;
+        model.texts = metaModel.texts;
         return model;
     }
 }
