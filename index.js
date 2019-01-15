7dd7b6b5425040f551470284c9bf2156c7916706
Tim Kilian <mail@timkilian.pl>
2019-01-15 16:32:09 +0100

    Change export

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-export { default } from './src/core/Plugin';
+import Plugin from './src/core/Plugin';
+
+import { MetaModel } from './src/ontology/MetaModel';
+import { Stylesheet } from './src/ontology/Stylesheet';
+import { ToolConfiguration } from './src/ontology/ToolConfiguration';
+
+
+export default {
+    Plugin,
+    Ontology: {
+        MetaModel,
+        Stylesheet,
+        ToolConfiguration
+    }
+};
