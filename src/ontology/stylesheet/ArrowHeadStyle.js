ac1e8524ac26a1b393ac992c153a7bb1aed81447
Tim Kilian <mail@timkilian.pl>
2019-02-25 20:04:19 +0100

    Add ref to arrow-head

--- src/ontology/stylesheet/ArrowHeadStyle.js
+++ src/ontology/stylesheet/ArrowHeadStyle.js
@@ -1,10 +1,14 @@
 export class ArrowHeadStyle {
     constructor (targetType) {
         this.targetType = targetType;
+        this.ref = { x: 0, y: 0 }
     }
 
     static fromJson (ontologyStyle) {
         const arrowHeadStyle = new ArrowHeadStyle(ontologyStyle['target-type']);
+        if (ontologyStyle['ref']) {
+            arrowHeadStyle.ref = ontologyStyle['ref'];
+        }
         arrowHeadStyle.representation = ontologyStyle['representation'];
         return arrowHeadStyle;
     }
