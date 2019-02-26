82da28be2a0ad4941a84cf1edffa4db2f89a8bfa
5kilian <5kilian@informatik.uni-hamburg.de>
2019-02-26 23:28:43 +0100

    Add regex to type

--- src/ontology/metamodel/Text.js
+++ src/ontology/metamodel/Text.js
@@ -1,9 +1,14 @@
 export class Text {
     constructor (type) {
         this.type = type;
+        this.regex = '.+';
     }
 
     static fromJson (raw) {
-        return new Text(raw.type);
+        const text = new Text(raw.type);
+        if (raw.regex) {
+            text.regex = raw.regex;
+        }
+        return text;
     }
 }
