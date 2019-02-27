c9705574101fe265755f3514fa1fd76ff05e0492
Tim Kilian <mail@timkilian.pl>
2019-02-27 13:16:50 +0100

    Implement TextStyle

--- src/ontology/Stylesheet.js
+++ src/ontology/Stylesheet.js
@@ -1,6 +1,7 @@
 import { ArrowHeadStyle } from './stylesheet/ArrowHeadStyle';
 import { ClassifierStyle } from './stylesheet/ClassifierStyle';
 import { RelationStyle } from './stylesheet/RelationStyle';
+import { TextStyle } from './stylesheet/TextStyle';
 
 
 export class Stylesheet {
@@ -26,6 +27,10 @@ export class Stylesheet {
         ontology['arrow-head-styles'].forEach((style) => {
             const arrowHeadStyle = ArrowHeadStyle.fromJson(style);
             stylesheet.styles[arrowHeadStyle.targetType] = arrowHeadStyle;
+        });
+        ontology['text-styles'].forEach((style) => {
+            const textStyle = TextStyle.fromJson(style);
+            stylesheet.styles[textStyle.targetType] = textStyle;
         });
         return stylesheet;
     }
--- src/ontology/stylesheet/TextStyle.js
+++ src/ontology/stylesheet/TextStyle.js
@@ -1,5 +1,14 @@
 export class TextStyle {
     constructor (targetType) {
         this.targetType = targetType;
+        this.position = 'center';
+    }
+
+    static fromJson (ontology) {
+        const textStyle = new TextStyle(ontology['target-type']);
+        if (ontology.position) {
+            textStyle.position = ontology.position;
+        }
+        return textStyle;
     }
 }
