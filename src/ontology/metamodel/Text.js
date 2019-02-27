6e33b620d8461716f815e3b16631c7ac4492e137
Tim Kilian <mail@timkilian.pl>
2019-02-27 13:17:09 +0100

    Add default value to Text

--- src/ontology/metamodel/Text.js
+++ src/ontology/metamodel/Text.js
@@ -1,11 +1,15 @@
 export class Text {
     constructor (type) {
         this.type = type;
+        this.default = '';
         this.regex = '.+';
     }
 
     static fromJson (raw) {
         const text = new Text(raw.type);
+        if (raw.default) {
+            text.default = raw.default;
+        }
         if (raw.regex) {
             text.regex = raw.regex;
         }
