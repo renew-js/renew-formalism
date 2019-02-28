f21672f5b55d0c83ca373159eb8a25c8cf1865b2
5kilian <5kilian@informatik.uni-hamburg.de>
2019-02-28 12:15:20 +0100

    Add boundingBox support

--- src/ontology/stylesheet/TextStyle.js
+++ src/ontology/stylesheet/TextStyle.js
@@ -1,6 +1,10 @@
 export class TextStyle {
     constructor (targetType) {
         this.targetType = targetType;
+        this.boundingBox = {
+            width: 220,
+            height: 28
+        };
         this.orientation = {
             position: 'center',
             margin: {
@@ -14,6 +18,9 @@ export class TextStyle {
 
     static fromJson (ontology) {
         const textStyle = new TextStyle(ontology['target-type']);
+        if (ontology['bounding-box']) {
+            textStyle.boundingBox = ontology['bounding-box'];
+        }
         if (ontology.orientation) {
             textStyle.orientation = ontology.orientation;
         }
