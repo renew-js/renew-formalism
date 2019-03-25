e58ea37c5f7a664acf3ba2e4240bf0863f2f1faa
Marc Richter <marc@richter.bz>
2019-03-25 13:51:17 +0100

    Add import/export methods

--- src/core/Plugin.js
+++ src/core/Plugin.js
@@ -2,9 +2,11 @@
  * @abstract
  */
 export default class Plugin {
+
     constructor () {
         this.type = null;
     }
+
     /**
      * @abstract
      * @return {MetaModel}
@@ -28,4 +30,23 @@ export default class Plugin {
     getToolConfiguration () {
         return null;
     }
+
+    /**
+     * @abstract
+     * @param  {object} additionalData
+     * @return {string}
+     */
+    getExport (additionalData) {
+        return null;
+    }
+
+    /**
+     * @abstract
+     * @param  {object} data
+     * @return {object}
+     */
+    import (data) {
+        return null;
+    }
+
 }
