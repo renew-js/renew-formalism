192d04e414f035fd2b7d42c84832ba5548ed4b35
Tim Kilian <mail@timkilian.pl>
2019-05-13 13:30:05 +0200

    Restructure labels -> targets in metamodel

--- src/ontology/metamodel/Relation.js
+++ src/ontology/metamodel/Relation.js
@@ -1,7 +1,7 @@
 export class Relation {
+
     constructor (type) {
         this.type = type;
-        this.labels = [];
         this.arrowStart = null;
         this.arrowEnd = null;
     }
@@ -11,7 +11,7 @@ export class Relation {
         relation.bind = raw.bind;
         relation.arrowStart = raw['arrow-start'];
         relation.arrowEnd = raw['arrow-end'];
-        raw.labels.forEach(label => relation.labels.push(label));
         return relation;
     }
+
 }
